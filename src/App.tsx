7385e126d157e27731369f55bb54849f823ee31a
adoll <20057155+ad0ll@users.noreply.github.com>
2024-02-04 11:40:37 -0500

    Initial commit w/ basic setup

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+import './index.tsx';
+
+function App() {
+
+    return (
+        <>
+            <h1 className="font-extrabold ml-10 ">DDMesh Schema Registry</h1>
+
+            <div className="flex flex-row">
+                <div className="flex flex-col w-1/2">
+                    Cool body
+                </div>
+            </div>
+        </>
+    );
+}
+
+export default App;
